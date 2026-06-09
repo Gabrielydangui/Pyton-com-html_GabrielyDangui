c10e17372214110113f2392e1c094d332f6e218b
Gabrielydangui <dangui.gabriely@escola.pr.gov.br>
2026-06-09 14:38:44 +0000

    terceira aula

--- app.py
+++ app.py
@@ -1,1 +1,25 @@
-from flask import Flask, render_template app = Flask()@app.route('/') def inicio(): return render_template('index.html') app.run(host='0.0.0.0', port=3000)
+from flask import Flask, render_template 
+
+app = Flask(__name__)
+
+@app.route('/')
+def inicio():
+   nome = "Turma de Programação"
+   curso = "Pyton + HTML"
+
+   return render_template(
+   'index.html',
+    nome = nome,
+    curso = curso
+    )
+
+@app.route ('/sobre')
+def sobre():
+  return """
+  <h1>Sobre a Turma</h1>
+  p>Esse projeto foi criado usando python e HTML.</p>
+  <a href="/">Voltar para o início</a>
+  """
+
+if _name_ == '_main_':
+   app.run(host='0.0.0.0', port=3000, debug=True)
